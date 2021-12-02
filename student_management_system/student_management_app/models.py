56df3176b4d5b5dbb16d6188d8d11aed4114d487
Ubuntu <favour@favour.gm2cicukewfu1fyg1xk3esznog.ax.internal.cloudapp.net>
2021-12-02 23:10:52 +0000

    database added

---
+++ student_management_system/student_management_app/models.py
@@ -0,0 +1,67 @@
+from django.db import models
+
+# Create your models here.
+class Admin(models.Model):
+    admin_id = models.CharField(max_length=255, primary_key=True)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects=models.Manager()
+
+class TheStaff(models.Model):
+    staff_id = models.CharField(max_length=255, primary_key=True)
+    full_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class Courses(models.Model):
+    course_id = models.CharField(max_length=255, primary_key=True)
+    course_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class Student(models.Model):
+    student_id = models.CharField(max_length=255, primary_key=True)
+    full_name = models.CharField(max_length=255)
+    gender = models.CharField(max_length=255)
+    address = models.TextField(max_length=255)
+    email = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    picture = models.FileField()
+    course_id = models.ForeignKey(Courses, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+class TaughtCourses(models.Model):
+    id = models.AutoField(primary_key=True)
+    course_id = models.ForeignKey(Courses, on_delete=models.CASCADE)
+    staff_id = models.ForeignKey(TheStaff, on_delete = models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add =True)
+    updated_at = models.DateTimeField(auto_now_add=True) 
+    objects = models.Manager()
+
+class StudentNotification(models.Model):
+    stud_not_id = models.AutoField(primary_key=True)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
+
+
+class StaffNotification(models.Model):
+    staf_not_id = models.AutoField(primary_key=True)
+    staff_id = models.ForeignKey(TheStaff, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    objects = models.Manager()
