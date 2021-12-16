544675e9bfb0da52fa851e9248828fdf821466cd
itagarf <itagarfavour@gmail.com>
2021-12-16 20:45:01 +0000

    main user added

--- student_management_system/student_management_app/admin.py
+++ student_management_system/student_management_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from student_management_app.models import MainUser
 
 # Register your models here.
+class UserModel(UserAdmin):
+    pass
+admin.site.register(MainUser,UserModel)
--- student_management_system/student_management_app/models.py
+++ student_management_system/student_management_app/models.py
@@ -1,21 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.dispatch.dispatcher import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+class MainUser(AbstractUser):
+    userData = ((1,"Admin"),(2,"Staff"),(3,"Student"))
+    userType  = models.CharField(default=1,choices=userData,max_length=50)
+
 class Admin(models.Model):
-    admin_id = models.CharField(max_length=50, primary_key=True)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
+    adm_id = models.CharField(max_length=50, primary_key=True)
+    admin = models.OneToOneField(MainUser,on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects=models.Manager()
 
 class TheStaff(models.Model):
     staff_id = models.CharField(max_length=50, primary_key=True)
-    full_name = models.CharField(max_length=50)
+    admin = models.OneToOneField(MainUser,on_delete=models.CASCADE)
     gender = models.CharField(max_length=50)
     address = models.CharField(max_length=255)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
@@ -30,11 +34,9 @@ class Courses(models.Model):
 
 class Student(models.Model):
     student_id = models.CharField(max_length=50, primary_key=True)
-    full_name = models.CharField(max_length=50)
+    admin = models.OneToOneField(MainUser,on_delete=models.CASCADE)
     gender = models.CharField(max_length=50)
     address = models.TextField(max_length=255)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
     picture = models.FileField()
     course_id = models.ForeignKey(Courses, on_delete=models.DO_NOTHING)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -69,7 +71,7 @@ class StaffNotification(models.Model):
 
 class AttendanceBook(models.Model):
     attendance_id= models.AutoField(primary_key= True)
-    subject_id=models.Foreignkey(TaughtCourses,on_delete=models.Do_Nothing)
+    subject_id=models.ForeignKey(TaughtCourses,on_delete=models.DO_NOTHING)
     attend_date=models.DateTimeField(auto_now_add= True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at= models.DateTimeField(auto_now_add = True)
@@ -78,8 +80,8 @@ class AttendanceBook(models.Model):
 
 class AttendanceBookReport(models.Model):
     attendancerprt_id = models.AutoField(primary_key=True)
-    student_id = models.Foreignkey(Student,on_delete=models.DO_NOTHING)
-    attendance_id = models.Foreignkey(AttendanceBook,on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student,on_delete=models.DO_NOTHING)
+    attendance_id = models.ForeignKey(AttendanceBook,on_delete=models.CASCADE)
     attendance_status = models.BooleanField(default=False)
     created_date =models.DateTimeField(auto_now_add= True)
     updated_date = models.DateTimeField(auto_now_add= True)
@@ -88,7 +90,7 @@ class AttendanceBookReport(models.Model):
 
 class Student_Leave_Report(models.Model):
     Leave_id=models.AutoField(primary_key=True)
-    Student_id=models.foreignKey(Student, on_delete=models.CASCADE)
+    Student_id=models.ForeignKey(Student, on_delete=models.CASCADE)
     Date_Of_Leave = models.CharField(max_length=255)
     Reason_For_Leave = models.TextField()
     Status_Of_Leave = models.BooleanField(default=False)
@@ -99,7 +101,7 @@ class Student_Leave_Report(models.Model):
 
 class StaffFeedback(models.Model):
     staff_feedback_id = models.AutoField(primary_key=True)
-    staff_id = models.foreignKey(TheStaff, on_delete=models.CASCADE)
+    staff_id = models.ForeignKey(TheStaff, on_delete=models.CASCADE)
     staff_feedback = models.TextField()
     Feedback_reply = models.TextField()
     Created_date= models.DateTimeField(auto_now_add=True)
@@ -109,7 +111,7 @@ class StaffFeedback(models.Model):
 
 class StudentFeedback(models.Model):
     stud_feedback_id = models.AutoField(primary_key=True)
-    student_id = models.foreignKey(Student, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
     stud_feedback = models.TextField()
     Feedback_reply = models.TextField()
     Created_date= models.DateTimeField(auto_now_add=True)
@@ -119,10 +121,32 @@ class StudentFeedback(models.Model):
 
 class Staff_Leave_Report(models.Model):
     Leave_id=models.AutoField(primary_key=True)
-    staff_id=models.foreignKey(TheStaff, on_delete=models.CASCADE)
+    staff_id=models.ForeignKey(TheStaff, on_delete=models.CASCADE)
     Date_Of_Leave = models.CharField(max_length=255)
     Reason_For_Leave = models.TextField()
     Status_Of_Leave = models.BooleanField(default=False)
     Created_Date= models.DateTimeField(auto_now_add=True)
     Updated_Date = models.DateTimeField(auto_now_add=True)
     objects = models.Manager()
+
+@receiver(post_save,sender=MainUser)
+def createUserProfile(sender,instance,created,**kwargs):
+    if created:
+        if instance.userType==1:
+            Admin.objects.create(admin=instance)
+        if instance.userType==2:
+            TheStaff.objects.create(admin=instance)
+        if instance.userType==3:
+            Student.objects.create(admin=instance)
+
+@receiver(post_save,sender=MainUser)
+def saveUserProfile(sender,instance,**kwargs):
+    if instance.userType==1:
+        instance.admin.save()
+    if instance.userType==2:
+        instance.thestaff.save()
+    if instance.userType==3:
+        instance.student.save()
+
+            
+
