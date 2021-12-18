391e6ad2cbee9e1ba70010b6be0a86057eb7d49e
Shamiya <10592024@mydbs.ie>
2021-12-19 05:19:27 +0530

    Add staff file to the admin template and update the adminViews

--- student_management_system/student_management_app/adminViews.py
+++ student_management_system/student_management_app/adminViews.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.urls import reverse
 
-from student_management_app.models import Courses
+from student_management_app.models import Courses, MainUser, TheStaff
 
 def adminHome(request):
     return render(request,"Admin/home.html")
@@ -50,3 +50,31 @@ def saveEditedCourse(request):
         except:
             messages.error(request,"Failed to Edit Course")
             return HttpResponseRedirect(reverse("editCourse",kwargs={"course_id":course_id}))
+
+def addStaff(request):
+    return render(request,"Admin/AddStaff.html")
+
+
+def save_Staff(request):
+    if request.method!="POST":
+        return HttpResponse("Method Not Allowed")
+    else:
+        first_name=request.POST.get("first_name")
+        last_name=request.POST.get("last_name")
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+        address=request.POST.get("address")
+        try:
+            user= MainUser.objects.create_user(username=username,password=password,email=email,last_name=last_name,first_name=first_name,user_type=2)
+            user.staffs.address=address
+            user.save()
+            messages.success(request,"Successfully Added Staff")
+            return HttpResponseRedirect(reverse("addStaff"))
+        except:
+            messages.error(request,"Failed to Add Staff")
+            return HttpResponseRedirect(reverse("addStaff"))
+
+def staff_Manage(request):
+    staffs= TheStaff.objects.all()
+    return render(request,"Admin/manage_staff_template.html",{"staffs":staffs})
